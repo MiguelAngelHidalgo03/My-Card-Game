7f4a999c16107cbd5b4e3b08115b89f253d2f2ea
KiRy15 <4180441@alu.murciaeduca.es>
2025-05-04 20:54:04 +0200

    cambio todos 04/05/2'25

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,8 +1,12 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext , useEffect} from 'react';
 import './Login.css';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { UserContext } from '../../context/UserContext';
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const Login = () => {
   const { login } = useContext(UserContext);
@@ -11,7 +15,37 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const handleGoogleLogin = async () => {
+    try {
+      // Iniciar sesión con Google
+      const { user, session, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+      });
 
+      if (error) {
+        console.error('Error al iniciar sesión con Google:', error);
+        setMessage(`Error al iniciar sesión con Google: ${error.message}`);
+        return;
+      }
+
+      // Enviar el token de acceso al backend para validar e iniciar sesión o registrar
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/google-auth`, {
+        access_token: session.access_token,
+      });
+
+      if (response.status === 200) {
+        // Almacenar la información del usuario (puedes usar contexto, localStorage, etc.)
+        console.log('Inicio de sesión exitoso:', response.data);
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Error al procesar el login con Google:', error);
+      setMessage('Error al iniciar sesión con Google');
+    }
+  };
+  
+  
+  
   const handleLogin = async (e) => {
     e.preventDefault();
 
@@ -63,6 +97,10 @@ const Login = () => {
           required
         />
         <button type="submit">Entrar</button>
+        <button onClick={handleGoogleLogin} className="google-btn">
+           Iniciar sesión / Registrarse con Google
+        </button>
+
         <p>¿No tienes cuenta? <Link to="/register">Regístrate aquí</Link></p>
         <p><Link to="/reset-request">¿Se te olvidó la contraseña?</Link></p>
       </form>
